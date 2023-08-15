9b09f1e6b9d02c8413b9880fce8fec6038608249
rileysong01 <rsong47@uwo.ca>
2023-08-15 19:13:23 -0400

    questions and timer working

--- script.js
+++ script.js
@@ -9,22 +9,25 @@ var liEl3;
 var liEl4;
 var scoreEl = document.querySelector("#score")
 var score = 0;
-var MCQuestionNumber = 0;
-var SAQuestionNumber = 0;
+var MCQuestionNumber;
+var SAQuestionNumber;
+var userAnswer;
+var formEl = document.querySelector("#input");
+var countdownTimer;
+var timeEl = document.querySelector('#time');
+var initialsEl = document.querySelector('#initials')
+var userInitials;
 
 var MCQuestionBank = [
-    {q: "1+1?", a1: "8", a2: "4", a3: "9", a4: "2", aCorrect: "2" },
-    {q: "fruit?", a1: "?", a2: "apple", a3: "?", a4: "cat", aCorrect: "apple" },
-    {q: "pompompurin?", a1: "fat", a2: "huh", a3: "skinny", a4: "hehe", aCorrect: "fat"},
-    {q: "hello kitty's nickname?", a1:"HK", a2:"stinky", a3: "daniel", a4: "kitty", aCorrect: "kitty"},
-    {q: "name of the imp with black ears?", a1:"crumi", a2:"calamity", a3: "kuromi", a4: "kuropi", aCorrect: "kuromi"}
+    { q: "1+1?", a1: "8", a2: "4", a3: "9", a4: "2", aCorrect: "2" },
+    { q: "2+2?", a1: "100", a2: "4", a3: "200", a4: "cat", aCorrect: "apple" },
+    { q: "3+3?", a1: "6", a2: "100", a3: "200", a4: "hehe", aCorrect: "6" },
 ]
 
 var SAQuestionBank = [
-    {q: "yes", a: "yeth"},
-    {q: "no", a: "naur"}
+    { q: "yesssss", aCorrect: "yes" },
+    { q: "nooooo", aCorrect: "no" }
 ]
-var countdownTimer;
 
 //click start button to start quiz
 startButtonEl.addEventListener("click", startQuiz)
@@ -32,38 +35,81 @@ startButtonEl.addEventListener("click", startQuiz)
 function startQuiz() {
     startButtonEl.style.display = "none";
     countdownTimer = 200;
+    setTime();
     MCQuestionNumber = 0;
+    SAQuestionNumber = 0;
     score = 0;
     nextMCQuestion();
 }
 
+function setTime() {
+    var timerInterval = setInterval(function () {
+        countdownTimer--;
+        timeEl.textContent = countdownTimer + " seconds left.";
+
+        //not working
+        if (countdownTimer === 0) {
+            clearInterval(timerInterval);
+    
+        }
+    }, 1000);
+}
+
+//displays next MC question
 function nextMCQuestion() {
     clearQuestion();
     if (MCQuestionNumber < MCQuestionBank.length) {
         MCQuestionSetUp(MCQuestionNumber);
         MCQuestionNumber++;
     } else {
-        console.log("move on to short answer!")
+        nextSAQuestion();
         console.log(score);
     }
 }
 
-// function nextSAQuestion() {
-//     clearQuestion();
-//     if (SAQuestionNumber < SAQuestionBank.length) {
-//         SAQuestionSetUp(SAQuestionNumber);
-//         SAQuestionNumber++;
-// }
+//displays next SA question
+function nextSAQuestion() {
+    clearQuestion();
+    if (SAQuestionNumber < SAQuestionBank.length) {
+        SAQuestionSetUp(SAQuestionNumber);
+        SAQuestionNumber++;
+    } else {
+        countdownTimer = 0;
+    }
+}
 
 function clearQuestion() {
     promptEl.textContent = "";
     ulEl.innerHTML = "";
     resultEl.textContent = ""
+    formEl.innerHTML = "";
 }
 //timer 
 
+var SAQuestionSetUp = function (SAQuestionNumber) {
+    promptEl.textContent = SAQuestionBank[SAQuestionNumber].q;
+    var inputEl = document.createElement("input");
+    inputEl.setAttribute("type", "text");
+    formEl.appendChild(inputEl)
 
-var MCQuestionSetUp = function(MCQuestionNumber) {
+    inputEl.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            userAnswer = (inputEl.value).toString();
+            if (userAnswer === SAQuestionBank[SAQuestionNumber].aCorrect) {
+                resultEl.textContent = "Correct!";
+                score++;
+            } else {
+                resultEl.textContent = "Incorrect";
+                countdownTimer -= 10
+            }
+            setTimeout(nextSAQuestion, 1000);
+        }
+    });
+
+}
+
+var MCQuestionSetUp = function (MCQuestionNumber) {
     promptEl.textContent = MCQuestionBank[MCQuestionNumber].q;
     var liEl1 = document.createElement("li");
     var liEl2 = document.createElement("li");
@@ -94,6 +140,7 @@ var MCQuestionSetUp = function(MCQuestionNumber) {
             score++;
         } else {
             resultEl.textContent = "Incorrect";
+            countdownTimer -= 10
         }
         setTimeout(nextMCQuestion, 1000);
     }
@@ -104,6 +151,25 @@ var MCQuestionSetUp = function(MCQuestionNumber) {
 //wrong answer subtracts time from clock
 
 //end of quiz (all questions answered OR timer hit 0)
+var endQuiz = function () {
+    promptEl.textContent = "Quiz Finished!";
+    resultEl.textContent = "your score" + score;
+    var getInitials = document.createElement("input")
+    getInitials.setAttribute("type", "text");
+
+    initialsEl.appendChild(getInitials);
+
+    getInitials.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            userInitials = (getInitials.value).toString();
+        }
+    });
+
+    console.log(userInitials);
+}
+
+
 
 //type my initials and save score
-//clear scores?
+//clear scores
